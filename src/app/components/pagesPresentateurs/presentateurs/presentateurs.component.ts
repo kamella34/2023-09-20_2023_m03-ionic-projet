2472851a8e420f9e61bd83d232b542fec6fda669
kamella34 <vergnaud.kamella@bbox.fr>
2023-10-01 17:29:29 +0200

    presentations

--- src/app/components/pagesPresentateurs/presentateurs/presentateurs.component.ts
+++ src/app/components/pagesPresentateurs/presentateurs/presentateurs.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PresentateursService } from 'src/app/components/shared/services/presentateurs.service';
 import { Presentateur } from '../../shared/models/presentateursModel';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Session } from '../../shared/models/sessionsModel';
+import { SessionsService } from '../../shared/services/sessions.service';
+
 
 @Component({
   selector: 'app-presentateurs',
@@ -10,14 +11,17 @@ import { Session } from '../../shared/models/sessionsModel';
   styleUrls: ['./presentateurs.component.scss'],
 })
 export class PresentateursComponent implements OnInit {
-    // session!: Session;
-  sessions: Session[] = [];
+ 
   presentateur!: Presentateur;
   id!: number;
-  nameSpeakerSession: string = "";
+  speakerName!: string;
+  speakerId!: number;
+  sessionSpeakers: number[] = [];
+  sessionTitle!: string;
+  sessionPresentateur!: string;
 
   constructor(private _presentateursService: PresentateursService, private _router: Router,
-    private _route: ActivatedRoute) { }
+    private _route: ActivatedRoute, private _sessionsService: SessionsService) { }
 
   ngOnInit() {
 
@@ -26,14 +30,29 @@ export class PresentateursComponent implements OnInit {
       this._presentateursService.findAllPresentateur().subscribe(data => {
         if (data.hasOwnProperty(this.id)) {
           this.presentateur = data[this.id];
-        } else {
-          console.error('not found')
+          this.speakerName = data[this.id].name;
+          this.speakerId = data[this.id].id;
+          console.log("SPEAKER iD " + this.speakerId);
+          console.log("SPEAKER NAME " + this.speakerName);
         }
+
+        this._sessionsService.findAllSession().subscribe(data => {
+          for (let idSession in data) {
+            this.sessionSpeakers = data[idSession].speakers;
+            this.sessionTitle = data[idSession].title;
+
+            if (this.sessionSpeakers != null) {
+              this.sessionSpeakers.forEach(element => {
+                if (element == this.speakerId) {
+                  this.sessionPresentateur = this.sessionTitle
+                }
+              });
+            }
+          }
+        })
+
       })
-
-    });
-
+    })
   }
-
-
 }
+
